d843a502dc587a4b5e36dd36fce9111998f3a8cf
Renato Dehnhardt <renatotkd@gmail.com>
2019-11-04 11:51:43 +0100

    Add repository

--- src/Abstracts/Controllers/Controller.php
+++ src/Abstracts/Controllers/Controller.php
@@ -5,9 +5,10 @@ namespace Porto\Abstracts\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 use Porto\Traits\CallableTrait;
 
-abstract class Controller extends LaravelBaseController
+abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, CallableTrait;
 }
