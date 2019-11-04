1d92e1d81f0fbeed30ed8e5d20f1c33156080fa2
Renato Dehnhardt <renatotkd@gmail.com>
2019-11-04 10:26:37 +0100

    Add fractal

--- src/Abstracts/Providers/MainServiceProvider.php
+++ src/Abstracts/Providers/MainServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace Porto\Abstracts\Providers;
 
+use Illuminate\Support\ServiceProvider;
 use Porto\Loaders\AliasesLoader;
 use Porto\Loaders\ProvidersLoader;
 
-abstract class MainServiceProvider extends LaravelAppServiceProvider
+abstract class MainServiceProvider extends ServiceProvider
 {
     use ProvidersLoader;
     use AliasesLoader;
