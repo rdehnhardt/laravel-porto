d591938af799c2f0eef4de3414bec5d42912bf90
Renato Dehnhardt <renatotkd@gmail.com>
2019-11-22 11:31:53 +0100

    FIX Middleware set

--- src/Providers/PortoServiceProvider.php
+++ src/Providers/PortoServiceProvider.php
@@ -15,7 +15,7 @@ class PortoServiceProvider extends MainServiceProvider
      * @var  array
      */
     protected $providers = [
-        AuditingServiceProvider::class
+        AuditingServiceProvider::class,
     ];
 
     /**
